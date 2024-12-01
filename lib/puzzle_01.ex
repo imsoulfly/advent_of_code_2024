46ed0f8f86d1e219d008ca55937d0407265f0850
Ingo Müller <ingo.mue@pm.me>
2024-12-01 18:49:05 +0100

    Solve puzzle 01

---
+++ lib/puzzle_01.ex
@@ -0,0 +1,28 @@
+defmodule Puzzle01 do
+  def solve do
+    File.read!("./priv/static/puzzle_01_input.txt")
+    |> String.split("\n", trim: true)
+    |> Enum.reduce({[],[]}, fn line, {left, right} ->
+      [l, r] = String.split(line, "   " )
+      {[String.to_integer(l) | left], [String.to_integer(r) | right]}
+    end)
+    |> sort()
+    |> sum_of_distances(0)
+  end
+
+  def sum_of_distances({[l | []], [r | []]}, d) do
+    d + distance(l, r)
+  end
+
+  def sum_of_distances({[l | left], [r | right]}, d) do
+    sum_of_distances({left, right}, d + distance(l, r))
+  end
+
+  defp distance(l, r) when l > r, do: l - r
+  # catch all for when l <= r
+  defp distance(l, r), do: r - l
+
+  defp sort({left, right}) do
+    {Enum.sort(left), Enum.sort(right)}
+  end
+end
